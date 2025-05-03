278094f6a55b0333e4df483424908b95160f6219
Michael Dennis <mdennis281@gmail.com>
2025-05-02 20:48:05 -0500

    push

--- core/osrs_client.py
+++ core/osrs_client.py
@@ -336,13 +336,14 @@ class RuneLiteClient(GenericWindow):
             tab: ToolplaneTab = ToolplaneTab.INVENTORY,
             min_confidence=0.97
         ):
+        top_crop = 10
         match = self.find_item(
             item_identifier,
             tab,
             min_confidence,
-            crop=(0,20,0,0) # crop top off
+            crop=(0,top_crop,0,0) # crop top off
         )
-        match.start_y = match.start_y - 5
+        match.start_y = match.start_y - 5 + top_crop
         match.start_x = match.start_x - 3
         match.end_y = match.start_y + 15
         match.end_x = match.start_x + 30
