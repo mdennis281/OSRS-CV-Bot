12816835792b804cd3638843c8485f8cc158ae50
Michael Dennis <mdennis281@gmail.com>
2025-06-04 09:58:31 -0500

    control changes

--- core/control.py
+++ core/control.py
@@ -1,5 +1,8 @@
 import time
 from functools import wraps
+from bots.core.cfg_types import BreakCfgParam
+from core.logger import get_logger
+
 
 class SingletonMeta(type):
     """A thread-safe implementation of a Singleton metaclass."""
@@ -15,13 +18,26 @@ class ScriptControl(metaclass=SingletonMeta):
     def __init__(self):
         self._terminate = False
         self._pause = False
-        self.break_until = 0
+        self.break_until: float = 0
+        self.break_config: BreakCfgParam = None
+        self.log = get_logger("ScriptControl")
         self.start_listener()
+
     
     def start_listener(self):
         """Start a thread to listen for termination and pause requests."""
         import threading
         threading.Thread(target=self._listen_for_control, daemon=True).start()
+
+
+    def propose_break(self):
+        """Propose break based on configuration."""
+        if self.break_config and self.break_config.should_break():
+            sec = int(self.break_config.break_duration.choose())
+            self.initialize_break(
+                sec
+            )
+            self.log.info(f"Sleeping for {sec} seconds.")
     def _listen_for_control(self):
         """Thread function to listen for control signals."""
         import keyboard
@@ -31,7 +47,10 @@ class ScriptControl(metaclass=SingletonMeta):
                 return
             if keyboard.is_pressed('page down'):
                 self.pause = not self.pause
-            time.sleep(0.1)
+                while keyboard.is_pressed('page down'):
+                    # Wait until the key is released
+                    time.sleep(0.1)
+            time.sleep(0.05)
 
     @property
     def terminate(self):
@@ -40,7 +59,7 @@ class ScriptControl(metaclass=SingletonMeta):
     @terminate.setter
     def terminate(self, value: bool):
         if self._terminate != value:
-            print(f"Terminate set to {value}")
+            self.log.info(f"Terminate set to {value}")
         self._terminate = value
 
     @property
@@ -50,12 +69,12 @@ class ScriptControl(metaclass=SingletonMeta):
     @pause.setter
     def pause(self, value: bool):
         if self._pause != value:
-            print(f"Pause {'enabled' if value else 'disabled'}")
+            self.log.info(f"Pause {'enabled' if value else 'disabled'}")
         self._pause = value
 
     def initialize_break(self, seconds: int):
         """Set the break duration without causing the caller to sleep."""
-        self.break_until = time.time() + seconds
+        self.break_until = time.time() + int(seconds)
 
     def guard(self, func):
         """
